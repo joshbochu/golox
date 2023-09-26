e92057c1790c97cac2e499c74af354b0c3756753
joshbochu <joshuatbochu+git@gmail.com>
2023-09-26 23:38:52 +0530

    Add runFile + stubs

--- cmd/lox/main.go
+++ cmd/lox/main.go
@@ -10,8 +10,25 @@ func main() {
 		fmt.Println("Usage: lox [script]")
 		os.Exit(64)
 	} else if len(os.Args) == 2 { // ./lox fileName
-		// runFile()
+		runFile(os.Args[1])
 	} else { // ./lox (interactive mode)
-		// runPrompt()
+		runPrompt()
 	}
+}
+
+func runFile(path string) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error %v\n", err)
+		os.Exit(65)
+	}
+	run(string(bytes))
+}
+
+func runPrompt() {
+	fmt.Println("Running in interactive mode")
+}
+
+func run(source string) {
+	fmt.Println("Running source...")
 }
