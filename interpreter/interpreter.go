0db04414065f7fd719099b324773855db3776b6b
joshbochu <joshuatbochu+git@gmail.com>
2023-10-17 00:59:48 +0530

    stub-interpreter

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,1 +1,27 @@
 package interpreter
+
+import (
+	"github.com/joshbochu/lox-go/expr"
+)
+
+type Interpreter struct{}
+
+func (i *Interpreter) VisitLiteralExpr(expr *expr.Literal) interface{} {
+	return expr.Value
+}
+
+func (i *Interpreter) VisitGroupingExpr(expr *expr.Grouping) interface{} {
+	return i.evaluate(expr.Expression)
+}
+
+func (i *Interpreter) VisitBinaryExpr(expr *expr.Binary) interface{} {
+	return nil
+}
+
+func (i *Interpreter) VisitUnaryExpr(expr *expr.Unary) interface{} {
+	return nil
+}
+
+func (i *Interpreter) evaluate(expr expr.Expr) interface{} {
+	return expr.Accept(i)
+}
